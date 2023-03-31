3872326a6efd1ea97334c42489c35c6ce2467f91
Kofi Lartey <lesliejoe2k2@gmail.com>
2023-03-31 15:52:13 +0100

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+data_base = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '@prettyflacko2k2',
+    auth_plugin = 'mysql_native_password',
+
+)
+
+# prepare a cursor object
+cursor_object = data_base.cursor()
+
+# create the database
+cursor_object.execute("CREATE DATABASE Flackodatabase")
+
+print('All done')
